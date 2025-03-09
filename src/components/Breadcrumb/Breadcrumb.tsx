418c32a7b48ba1a407e32229bc1dc9926cc6d2f4
yuma0306 <yuma0306895@gmail.com>
2025-03-09 23:18:30 +0700

    css整理

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -10,20 +10,18 @@ type Props = {
 
 export const Breadcrumb = ({ items }: Props) => {
 	return (
-		<nav className={styles.breadcrumb}>
-			<ul className={styles.list}>
-				{items.map((item, index) => (
-					<li key={index} className={styles.item}>
-						{index === items.length - 1 ? (
-							<span className={styles.text}>{item.text}</span>
-						) : (
-							<Link className={styles.link} href={item.link}>
-								{item.text}
-							</Link>
-						)}
-					</li>
-				))}
-			</ul>
-		</nav>
+		<ul className={styles.list}>
+			{items.map((item, index) => (
+				<li key={index} className={styles.item}>
+					{index === items.length - 1 ? (
+						<span className={styles.text}>{item.text}</span>
+					) : (
+						<Link className={styles.link} href={item.link}>
+							{item.text}
+						</Link>
+					)}
+				</li>
+			))}
+		</ul>
 	);
 };
